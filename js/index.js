25f88feaf4c0d87a4f78bfea6cb50a147a696c0d
Sylwia <sylviaskovron@gmail.com>
2018-10-15 20:45:59 +0200

    init

---
+++ js/index.js
@@ -0,0 +1,157 @@
+$(document).ready(function(){
+
+	// hamburger
+	var hamburger = $(".menu-toggle");
+	var menu = $(".page-menu");
+	var body = $("body");
+	hamburger.on("click", function(){
+		$(this).toggleClass("active");
+		menu.toggleClass("show");
+		body.toggleClass("hide");
+	});
+
+	$(".page-menu a").on("click", function(){
+		hamburger.removeClass("active");
+		menu.removeClass("show");
+		body.removeClass("hide");
+	});
+
+
+	// scroll down
+	$("#scroll-down").click(function() {
+    	$('html, body').animate({
+        	scrollTop: $("#about-me").offset().top
+    	}, 1000, "linear");
+ 	});
+
+
+	// PRELOADER
+	var preloader = $("#preloader");
+	setTimeout(removePreloader, 1000);
+	function removePreloader(){
+		preloader.fadeOut(1000);
+	}
+
+	// iphone anim
+	var iphone = $(".iphone");
+	var iphone_shadow = $(".iphone-container")
+	iphone.on("mousemove", function(e){
+  		var centerX = $(this).width()/2;
+  		var moveX = centerX - e.offsetX;
+  		var centerY = $(this).width()/2;
+  		var moveY = centerY - e.offsetY;
+  		$(this).css({
+  			'transform': 'rotateY('+ moveX/10 +'deg) rotateX(' + moveY/15 + 'deg)'
+  		});
+  		iphone.addClass('stop-anim');
+	});
+
+	iphone.on("mouseout", function(e){
+  		iphone.attr("style", "transform: rotateY(0deg) rotateX(0deg);-webkit-transform: rotateY(0deg) rotateX(0deg);-moz-transform: rotateY(0deg) rotateX(0deg)").addClass("trans anim").removeClass('stop-anim');
+  		iphone.removeClass('stop-anim');
+	});
+
+
+	// FORM ANIMATION
+	$("input:not([type='submit']), textarea").on("focus", function(){
+		$(this).parent($(".input-group")).addClass("hl");
+	});
+	$("input:not([type='submit']), textarea").on("focusout", function(){
+		$(this).parent($(".input-group")).removeClass("hl");
+	});
+
+
+	// type.js
+	var typed = new Typed('.type', {
+	  strings: ["looove", "was born"],
+	  typeSpeed: 300,
+	  backSpeed: 100,
+	  fadeOut: true,
+	  loop: true,
+	  cursorChar: '_',
+      shuffle: true,
+	});
+
+	var typed2 = new Typed('.console-type', {
+	    strings: ['npm skills^1000\n `HTML5` ^2000\n `Fetching from source...`'],
+	   	typeSpeed: 40,
+    	backSpeed: 0,
+    	loop: false
+	});
+
+
+
+	// smooth scrolling for links
+	$(document).on('click', 'a[href^="#"]', function (event) {
+	    event.preventDefault();
+	    $('html, body').animate({
+	      scrollTop: $($.attr(this, 'href')).offset().top
+	     }, 1000);
+	});
+
+
+
+
+	// Portfolio
+	var projects = $(".project");
+	var projectsNumber = $(".project").length;
+	var projectDesc = $(".project-details");
+	var projectsNumberDesc = $(".project").length;
+	$(".projects-number-all").text(projectsNumber);
+
+
+	$("#portfolio button").on("click", function(){
+
+		var activeProject = $(".project.active-project");
+		var prev = $(".project.active-project").prev();
+		var next = $(".project.active-project").next();
+
+		var activeProjectDesc = $(".project-details.active-project");
+		var prevDesc = $(".project-details.active-project").prev();
+		var nextDesc = $(".project-details.active-project").next();
+
+
+		if(this.id == "next"){
+			console.log(nextDesc.index() + " / " + projectsNumberDesc);
+			if( next.index() < projectsNumber){
+				next.removeClass("to-bottom").addClass("active-project");
+				setTimeout(function(){
+					activeProject.removeClass("active-project").addClass("to-top");
+					$(".projects-number-this").text(next.index() + 1);
+				}, 500);
+			}
+			if( nextDesc.index() === -1){
+				return false;
+			}
+			if( nextDesc.index() < projectsNumberDesc){
+				nextDesc.removeClass("to-bottom").addClass("active-project");
+				setTimeout(function(){
+					activeProjectDesc.removeClass("active-project").addClass("to-top");
+				}, 500);
+			} else {
+				return false;
+			}
+		} 
+		if (this.id == "prev"){
+			if($(".active-project").index() === 0){
+				return false;
+			}
+			if( prev.index() < projectsNumber){
+				prev.removeClass("to-top").addClass("active-project");
+				setTimeout(function(){
+					activeProject.removeClass("active-project").addClass("to-bottom");
+					$(".projects-number-this").text(prev.index() + 1);
+				}, 1000);
+			}
+			if( prevDesc.index() < projectsNumberDesc){
+				prevDesc.removeClass("to-top").addClass("active-project");
+				setTimeout(function(){
+					activeProjectDesc.removeClass("active-project").addClass("to-bottom");
+				}, 1000);
+			} else {
+				return false;
+			}
+		}
+	});
+
+});
